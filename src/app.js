f306c8e66f3dd1de351c55a5d1af1834c7bf62f1
dev-ng <astroniki@gmail.com>
2023-11-22 12:11:02 +0200

    Add react dependencies + babel configuration

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-console.log("App.js");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const template = <p>JSX test</p>;
+ReactDOM.render(template, document.getElementById("app"));
