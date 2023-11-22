d003005de9ad65617c2ce7ad419dafea5e27cb51
dev-ng <astroniki@gmail.com>
2023-11-22 14:31:17 +0200

    Add modal to show selected option instead of alert

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,10 +3,12 @@ import AddOption from "./AddOption";
 import Header from "./Header";
 import Action from "./Action";
 import Options from "./Options";
+import OptionModal from "./OptionModal";
 
 class IndecisionApp extends React.Component {
   state = {
     options: [],
+    selectedOption: undefined,
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -32,9 +34,14 @@ class IndecisionApp extends React.Component {
     this.setState(() => ({ options: [] }));
   };
 
+  handleClearSelectedOption = () => {
+    this.setState(() => ({ selectedOption: undefined }));
+  };
+
   handlePick = () => {
     const optionIndex = Math.floor(Math.random() * this.state.options.length);
-    alert(this.state.options[optionIndex]);
+    const selectedOption = this.state.options[optionIndex];
+    this.setState(() => ({ selectedOption }));
   };
 
   handleAddOption = (option) => {
@@ -73,6 +80,10 @@ class IndecisionApp extends React.Component {
           handleDeleteOption={this.handleDeleteOption}
         />
         <AddOption handleAddOption={this.handleAddOption} />
+        <OptionModal
+          selectedOption={this.state.selectedOption}
+          handleClearSelectedOption={this.handleClearSelectedOption}
+        />
       </div>
     );
   }
